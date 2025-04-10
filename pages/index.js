d82946b39629bd746cc4ab14a77f1e2616b07b0e
Sensei7beats <sensei7beats@gmail.com>
2025-04-10 13:27:55 -0400

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ backgroundColor: '#111', color: '#fff', minHeight: '100vh', padding: '2rem' }}>
+      <h1 style={{ fontSize: '2.5rem' }}>Welcome to Sensei7Beats</h1>
+      <p style={{ marginTop: '1rem' }}>This is your custom beat selling platform.</p>
+    </div>
+  );
+}
